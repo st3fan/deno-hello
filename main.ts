d0e261d3111e2c720b1567900bb48a190be341c3
Stefan Arentz <stefan@arentz.ca>
2022-06-04 18:41:23 -0400

    Updates

---
+++ main.ts
@@ -0,0 +1,11 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+
+serve((_request: Request) => {
+    return new Response("<h1>Hello World!</h1>", {
+        headers: { "content-type": "text/html" },
+    });
+});
