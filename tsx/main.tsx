3843c333de7a8b887c4ba75449632e3c8abf648b
Stefan Arentz <stefan@arentz.ca>
2022-06-04 19:14:32 -0400

    Updates

---
+++ tsx/main.tsx
@@ -0,0 +1,30 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+import { h, renderSSR } from "https://deno.land/x/nano_jsx@v0.0.20/mod.ts";
+
+function App() {
+  return (
+    <html>
+      <head>
+        <title>Hello from JSX</title>
+      </head>
+      <body>
+        <h1>Hello world</h1>
+      </body>
+    </html>
+  );
+}
+
+function handler(_request: Request) {
+  const html = renderSSR(<App />);
+  return new Response(html, {
+    headers: {
+      "content-type": "text/html",
+    },
+  });
+}
+
+serve(handler);
