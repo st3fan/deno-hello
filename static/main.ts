19f27e45ab182e9b7bc48932a2e63e3384976d7e
Stefan Arentz <stefan@arentz.ca>
2022-06-04 19:28:09 -0400

    Updates

---
+++ static/main.ts
@@ -0,0 +1,32 @@
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+
+async function handleRequest(request: Request): Promise<Response> {
+    const { pathname } = new URL(request.url);
+    switch (pathname) {
+        case "index.html": {
+            const contents = await Deno.readFile("./static/index.html");
+            return new Response(contents, {
+                headers: {
+                  "content-type": "text/html",
+                },
+              });
+        }
+        case "styles.css": {
+            const contents = await Deno.readFile("./static/styles.css");
+            return new Response(contents, {
+                headers: {
+                  "content-type": "text/css",
+                },
+              });
+        }
+        default: {
+            return new Response(`<h1>Not found</h1>`, {
+                status: 404,
+                statusText: "Not found",
+                headers: { "content-type": "text/html" },
+            })
+        }
+    }
+}
+
+serve(handleRequest);
